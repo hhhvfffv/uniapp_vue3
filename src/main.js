cd4f5cf25c3fc0bfed444ac3b72b8f1ef70ca5bb
hhhvfffv <168108171+hhhvfffv@users.noreply.github.com>
2025-03-17 21:13:33 +0800

    安装了pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import {
-	createSSRApp
-} from "vue";
+import { createSSRApp } from "vue";
 import App from "./App.vue";
+
+// 引入pinia
+import { createPinia } from 'pinia'
 export function createApp() {
+	const pinia = createPinia()
 	const app = createSSRApp(App);
+	app.use(pinia)
 	return {
 		app,
+		pinia
 	};
 }
