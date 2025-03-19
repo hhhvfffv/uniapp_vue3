c3f08a3ecfbc0f96722f8e5494f3a0b371779aa6
hhhvfffv <168108171+hhhvfffv@users.noreply.github.com>
2025-03-19 21:08:16 +0800

    添加了公用组件在pages.json自动导入配置

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,12 @@
 <template>
  <CustomNavbar />
+ <XtxSwiper />
  <view class="index">index</view>
 </template>
 
 <script setup>
 //引入组件
-import CustomNavbar from '../componets/CustomNavbar.vue'
+import CustomNavbar from '../components/CustomNavbar.vue'
 
 import { ref } from 'vue';
 import { onLoad, onShow, onReady } from '@dcloudio/uni-app';
