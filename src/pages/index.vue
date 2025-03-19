fb2bae29770b296cf5ce906d24c413324372a94d
hhhvfffv <168108171+hhhvfffv@users.noreply.github.com>
2025-03-19 19:54:06 +0800

    写了主页的CustomNavbar组件，添加了项目图片文件

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,42 +1,19 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+ <CustomNavbar />
+ <view class="index">index</view>
 </template>
 
 <script setup>
+//引入组件
+import CustomNavbar from '../componets/CustomNavbar.vue'
+
 import { ref } from 'vue';
 import { onLoad, onShow, onReady } from '@dcloudio/uni-app';
-const title = ref('Hello World');
+
+
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+<style lang="scss">
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
 </style>
